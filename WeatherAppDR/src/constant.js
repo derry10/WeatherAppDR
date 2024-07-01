252424b7ba4800c5726fa897dc91d20fddaead55
derry10 <122221930+derry10@users.noreply.github.com>
2024-07-01 22:20:52 +0700

    Update constant.js

--- WeatherAppDR/src/constant.js
+++ WeatherAppDR/src/constant.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-export const API_KEY = "API Key yang sudah didapatkan";
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'fe42548084926ffe29c409208b52bae5';
